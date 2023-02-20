278dc3edaef93d89e6c74d12bef156a6e6f04f0f
Donatas Drobuzas <donatas.drobuzas@gmail.com>
2023-02-20 15:38:15 +0200

    feat: morgan library added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv' 
 dotenv.config();
 import express from 'express';
+import morgan from 'morgan';
 
 const {SERVER_PORT, SERVER_DOMAIN} = process.env;
 
@@ -8,7 +9,8 @@ if(SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
     throw new Error(" Please define constants in '.env' file" )
 }
 const server = express();
-server.use(express.static('public'))
+server.use(express.static('public'));
+server.use(morgan('tiny'));
 
 server.listen(SERVER_PORT, () => {
     console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`)
